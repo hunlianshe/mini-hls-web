63a95bef7553063290689c5c628010bca7265f1b
Lisa5 <1348894516@qq.com>
2019-09-08 20:01:47 +0800

    feat: register

--- miniprogram/interface/user.ts
+++ miniprogram/interface/user.ts
@@ -4,17 +4,17 @@ interface User {
   gender?: number,
   birth?: string,
   height?: number,            // 身高
-  month_incomeRange?: string, //月收入
-  work_province?: string,     // 工作 省
-  work_city?: string,         // 工作 市
-  work_region?: string,       // 工作 区
+  salary?: string,            //月收入
+  workProvince?: string,      // 工作 省
+  workCity?: string,          // 工作 市
+  workRegion?: string,        // 工作 区
   education?: string,         // 学历
-  marital_status?: boolean,   // 是否已婚 没有值->请选择 - boolean
-  with_child?: boolean,       // 是否有孩子
-  want_child?: boolean,       // 是否想要孩子
-  job_general?: string,       // 工作类型的大类别
-  job_detail?: string,        // 工作类型的小类别
-  haveHouse?: string,        // 是否有房子
+  isMarriage?: boolean,       // 是否已婚 没有值->请选择 - boolean
+  hasChild?: boolean,         // 是否有孩子
+  wantChild?: boolean,        // 是否想要孩子
+  jobGeneral?: string,        // 工作类型的大类别
+  jobDetail?: string,         // 工作类型的小类别
+  haveHouse?: string,         // 是否有房子
   phone?: string,             // 电话号码
   language?: string,  
   city?: string,
