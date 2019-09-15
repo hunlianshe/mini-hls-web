b26e367464a09ada0cdd46756385e936d49b52fa
Lisa5 <1348894516@qq.com>
2019-09-15 21:56:39 +0800

    feat: xlcs

--- miniprogram/pages/registerXz/registerXz.ts
+++ miniprogram/pages/registerXz/registerXz.ts
@@ -114,7 +114,6 @@ Page({
         submitDisable: true
       });
       if (result.code === 200) {
-        // utils.showModal('更新成功')
         setTimeout(() => {
           wx.switchTab({
             url: `../myHome/myHome`,
@@ -149,20 +148,6 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
 
   },
 })
