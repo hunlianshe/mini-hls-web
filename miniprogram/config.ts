94fb2341e9b4186d1dedeeedc023509f85a1511b
刘祖宽 <18818216454@163.com>
2019-09-24 21:09:26 +0800

    qrcode

--- miniprogram/config.ts
+++ miniprogram/config.ts
@@ -3,9 +3,9 @@
 
 var host = "14592619.qcloud.la"
 // var socket = "https://www.peajs.top"
-// var hostUrl = "http://localhost:8009"
+var hostUrl = "http://localhost:8009"
 
-var hostUrl = "https://www.peajs.top/testhls"
+// var hostUrl = "https://www.peajs.top/testhls"
 let ak = 'xsiYQN0VwrBHvxmf42BGdxFiTQgqBC4w';
 var fileHost = "https://test-buycarshop.oss-cn-beijing.aliyuncs.com"
 
