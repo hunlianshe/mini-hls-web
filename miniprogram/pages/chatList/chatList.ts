e8e0dccd812beee2fabae0711837aa02fa1b9ea1
Lisa5 <1348894516@qq.com>
2020-10-18 20:41:52 +0800

    feat: 聊天列表

--- miniprogram/pages/chatList/chatList.ts
+++ miniprogram/pages/chatList/chatList.ts
@@ -1,7 +1,36 @@
 
 Page({
   data: {
-    userList: [], // 用户列表
+    userList: [
+      {
+        "_id": "5f66e025bb97350949c52a97",
+        "userIds": [
+          "oHgB55LJ1wGo2QqEYxgo8tLMxL4A",
+          "oHgB55AlhKqR7azr85YYBwfIE9EQ"
+        ],
+        "createId": "oHgB55AlhKqR7azr85YYBwfIE9EQ",
+        "createdAt": "2020-09-20T04:52:53.115Z",
+        "updatedAt": "2020-09-20T07:57:43.815Z",
+        "lastMessage": {
+          "cid": "5f66e025bb97350949c52a97",
+          "type": 1,
+          "msg": "hello lisa",
+          "status": [
+            {
+              "openid": "oHgB55LJ1wGo2QqEYxgo8tLMxL4A",
+              "msgUnread": true
+            },
+            {
+              "openid": "oHgB55AlhKqR7azr85YYBwfIE9EQ",
+              "msgUnread": true
+            }
+          ],
+          "from": "oV2Js5THL6EdzDahAxCTxFoXyjHk",
+          "fromName": "刘祖宽",
+          "fromAvatarUrl": "https://wx.qlogo.cn/mmopen/vi_32/DYAIOgq83erbAfZ10K9richIBTz7kDsA9lUsVyzicShXgxw9zeMfekOUk6s7JGOVtCza5veuxvibsJyOOgVICwpPQ/132"
+        }
+      }
+    ], // 用户列表
     matcherImage: '../../public/image/matcher.png',
   },
 
@@ -9,20 +38,22 @@ Page({
 
   },
 
+  /** 去聊天 */
+  goChat(e: any) {
+    const { openid } = e.currentTarget.dataset;
+    wx.navigateTo({
+      url: `../chat/chat?openid=${openid}`,
+    })
+  },
+
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
